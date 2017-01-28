b1c711a684054853d4c82949cdfd01c1dcd12ac7
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2017-01-28 22:52:50 +0100

    implementing threejs basics

--- src/client/Game.ts
+++ src/client/Game.ts
@@ -3,10 +3,34 @@ import * as Networking from "./Networking";
 export class Game implements Networking.INetworkHandler
 {
     private connection : Networking.Connection
+    private scene : THREE.Scene
+    private renderer : THREE.Renderer;
+    private camera : THREE.PerspectiveCamera;
     constructor()
     {
         this.connection = new Networking.Connection();
         this.connection.SetHandler(this);
+
+        this.scene = new THREE.Scene();
+        this.renderer = new THREE.WebGLRenderer();
+        this.camera = new THREE.PerspectiveCamera(85, 1, 0.1, 1000);
+    }
+
+    public GetWebGLElement() : HTMLElement
+    {
+        return this.renderer.domElement;
+    }
+
+    public SetResolution(width : number, height: number) : any
+    {
+        this.renderer.setSize(width, height);
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
+    }
+
+    public Start() : void
+    {
+        this.render();
     }
 
     OnConnect()
@@ -17,5 +41,11 @@ export class Game implements Networking.INetworkHandler
     OnMessage(data : Networking.IMessage)
     {
 
+    }
+
+    private render() : void
+    {
+        requestAnimationFrame(() => this.render());
+        this.renderer.render(this.scene, this.camera);
     }
 }
